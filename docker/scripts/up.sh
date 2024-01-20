40a5b927f7718e2a6894b00b1207d9c9c3dc932b
valentinconan <valentin.conan@me.com>
2024-01-20 14:22:11 +0100

    fix: change docker service name, image name and public port. Improve up script

--- docker/scripts/up.sh
+++ docker/scripts/up.sh
@@ -14,13 +14,13 @@ docker network create share-exchange
 set -e
 
 if [[ ! " ${excludes[*]} " =~ " proxy " ]]; then
-  docker-compose -f proxy/docker-compose.proxy.yml up -d &
+  docker-compose -f proxy/docker-compose.proxy.yml up -d
 fi
 
 if [[ ! " ${excludes[*]} " =~ " share-frontend " ]]; then
-  docker-compose -f frontend/docker-compose.yml up -d &
+  docker-compose -f frontend/docker-compose.yml up -d
 fi
 
 if [[ ! " ${excludes[*]} " =~ " share-backend " ]]; then
-  docker-compose -f backend/docker-compose.yml up -d &
+  docker-compose -f backend/docker-compose.yml up -d
 fi
