2aba79f12e26cd3d526a8bd0047a44a5c8694594
valentinconan <valentin.conan@me.com>
2024-01-20 23:35:42 +0100

    reformat project, add db configuration

---
+++ project/backend/api/ormconfig.ts
@@ -0,0 +1,17 @@
+import {ConnectionOptions} from 'typeorm';
+import * as process from "process";
+import {Holding} from "./src/api/shares/entities/holding.entity";
+import {Order} from "./src/api/shares/entities/order.entity";
+import {ShareHolder} from "./src/api/shares/entities/shareholder.entity";
+import {Share} from "./src/api/shares/entities/share.entity";
+
+let url: string = process.env.DB_URL;
+
+url = url ?? "postgres://postgres:password@localhost:5432/nest"
+export default {
+    type: 'postgres',
+    url: url,
+    database: 'filmash',
+    entities: [Holding, Order, Share, ShareHolder],
+    synchronize: true,
+} as ConnectionOptions;
--- project/backend/api/src/api/shares/dto/create-share.dto.ts
+++ project/backend/api/src/api/shares/dto/create-share.dto.ts
@@ -1,1 +1,25 @@
-export class CreateShareDto {}
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+
+export class CreateShareDto {
+
+
+    constructor(name: string, quantity: number, price: number) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+
+    @IsNumber()
+    @IsNotEmpty()
+    quantity: number
+
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number
+}
