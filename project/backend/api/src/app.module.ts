cde2f9cec6d0241d272f51dab103a16318764133
valentinconan <valentin.conan@me.com>
2024-01-20 22:27:27 +0100

    add base entity and db configuration

--- project/backend/api/src/app.module.ts
+++ project/backend/api/src/app.module.ts
@@ -1,13 +1,16 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {SharesModule} from './api/shares/shares.module';
+import config from "../ormconfig";
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot(config),
+        SharesModule],
     controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {
 }
-
-//todo vco add module shares here
