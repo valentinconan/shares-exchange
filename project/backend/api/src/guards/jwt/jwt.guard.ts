88a20491eb424a0d2d576457d214b7dfdae7e612
valentinconan <valentin.conan@me.com>
2024-01-21 14:05:51 +0100

    enhancement: improve error management

--- project/backend/api/src/guards/jwt/jwt.guard.ts
+++ project/backend/api/src/guards/jwt/jwt.guard.ts
@@ -1,8 +1,9 @@
-import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common';
+import {CanActivate, ExecutionContext, HttpException, Injectable} from '@nestjs/common';
 import {Request} from 'express';
 import {Reflector} from "@nestjs/core";
 import {HttpService} from '@nestjs/axios'
-import {map, Observable} from "rxjs";
+import {catchError, map, Observable} from 'rxjs';
+import {AxiosError} from 'axios';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
@@ -27,13 +28,14 @@ export class JwtGuard implements CanActivate {
         //precheck of authorization header presence
         const token = this.extractTokenFromHeader(request);
 
-        console.log(token)
+        if (!token) {
+            console.error('No authorization header found')
+            throw new HttpException('forbidden', 403);
+        }
+
         //then call user module in order to validate token
-
-
         return this.validateToken(token).pipe(
             map((isValid) => {
-                console.log(isValid)
                 return isValid;
             }),
         );
@@ -44,7 +46,26 @@ export class JwtGuard implements CanActivate {
         return this.httpService.post("http://localhost:3001/validate-token", {token}).pipe(
             map((response) => {
                 return response.status >= 200 && response.status <= 299 && response.data.valid;
-            }));
+            }),
+            catchError((error: AxiosError) => {
+                // Traitez ici l'erreur
+                if (error.response) {
+                    // L'appel a été fait et le serveur a répondu avec un code d'erreur
+                    console.error('Erreur de réponse:', error.response.data);
+                    throw new HttpException('forbidden', 403);
+                } else if (error.request) {
+                    // L'appel a été fait mais aucune réponse n'a été reçue
+                    console.error('Aucune réponse reçue:', error.request);
+                    throw new HttpException(error.request.data.error, error.request.status);
+                } else {
+                    // Une erreur s'est produite lors de la configuration de la requête
+                    console.error('Erreur de configuration de la requête:', error.message);
+                    throw new HttpException('Bad request', 400);
+                }
+
+                // Renvoyer une observable d'erreur pour que le flux puisse être interrompu
+                throw new HttpException('Bad request', 400);
+            }),);
     }
 
     private extractTokenFromHeader(request: Request): string | undefined {
