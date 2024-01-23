ba69bd4e8d89d9a38619214c4a34c5786fb89854
valentinconan <valentin.conan@me.com>
2024-01-23 21:21:03 +0100

    enhancement: improve error handler, adding logs

--- project/backend/api/src/filters/error-handler/error-handler.filter.ts
+++ project/backend/api/src/filters/error-handler/error-handler.filter.ts
@@ -1,8 +1,11 @@
-import {ArgumentsHost, Catch, ExceptionFilter, HttpStatus} from '@nestjs/common';
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger} from '@nestjs/common';
 import {Response} from 'express';
 
 @Catch()
 export class ErrorHandlerFilter<T> implements ExceptionFilter {
+
+    private readonly logger = new Logger(ErrorHandlerFilter.name);
+
     catch(exception: T, host: ArgumentsHost) {
 
         const response: Response = host.switchToHttp().getResponse<Response>();
@@ -12,8 +15,14 @@ export class ErrorHandlerFilter<T> implements ExceptionFilter {
                 statusCode: HttpStatus.CONFLICT,
                 message: 'Entity already exist. Please check your data',
             });
+        } else if (exception instanceof HttpException) {
+            response.status(exception.getStatus()).json({
+                statusCode: exception.getStatus(),
+                message: exception.getResponse(),
+            });
         } else {
-            // Gérer d'autres erreurs si nécessaire
+            this.logger.error(exception, exception['stack'])
+
             response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
                 statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
                 message: 'Unexpected error occurred.',
